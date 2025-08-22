a284c55cc8e8fccbde15a825aeabad43437f70c9
FaConKnight <chanatip35@gmail.com>
2025-08-22 18:22:33 +0700

    newversion

--- pomphone/line/line_config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$channelSecret = 'f7b887f0ac567200de61a3a0b8f9f46f';
-$accessToken = 'b2Ef1Gsg7mPp3+i9r0Lr8F2Lx9sUBuCu1wTstwJtJgkfL2DfJsMi15BRKPCdWEndn+2E+WolSG62hHqU5fR/oaAPpwArvm9e0GiJXs8x6yIjBGSm3tyqnsFEOHKqkObeLDkPruvsqSvLuCAGYw7/hwdB04t89/1O/w1cDnyilFU=';
